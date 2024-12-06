662607940079d8b794b8d2719709128657f6aaef
jadhavdurgesh <decode749@gmail.com>
2024-12-07 00:49:51 +0530

    Initial Commit

---
+++ demoAppSwift/demoAppSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoAppSwiftApp.swift
+//  demoAppSwift
+//
+//  Created by DURGESH GANPAT JADHAV on 07/12/24.
+//
+
+import SwiftUI
+
+@main
+struct demoAppSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
